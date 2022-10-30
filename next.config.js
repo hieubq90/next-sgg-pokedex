bb2c6ea624a9eedda65461e2938655e1c938e6ab
hieubq90 <hieubq90@gmail.com>
2022-10-30 09:14:29 +0700

    setup tailwind & sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
+  // configuration for Sass loader
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'components'), path.join(__dirname, 'pages')]
+  },
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig
