d9954471ee9c319c0f215c7d2e30911d62613f96
hieubq90 <hieubq90@gmail.com>
2022-10-30 15:17:15 +0700

    update Pokemon Card style

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import React, {useMemo} from "react";
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { Pokemon } from "../types/Pokemon";
 
@@ -11,18 +12,25 @@ const PokemonCard: React.FunctionComponent<{
 
     return (
         <div className="w-48 p-2 mb-5 mx-4 flex flex-col border border-gray-200 rounded-md shadow-md">
-            <div className="">
-                <span>{ `#${pokemon.num.toString().padStart(3, '0')}` }</span>
+            <div className="font-light text-slate-300">
+                { `#${pokemon.num.toString().padStart(3, '0')}` }
             </div>
-            <div className="relative w-40 h-44">
-                <Image alt="sprite" src={pokemon.sprite} fill />
+            <div className="relative w-40 h-44 mx-auto">
+                <Image alt="sprite" src={pokemon.sprite} fill sizes="160px" />
             </div>
-            <div className="">
-                {pokemon.name}
+            <div className="font-semibold text-center text-lg text-blue-500">
+                <Link href={`/${pokemon.id}`}>
+                    {pokemon.name}
+                </Link>
             </div>
-            <div className="">
+            <div className="flex justify-center text-sm font-light">
                 {pokemonTypes.map((item: string, index: number) => (
-                    <span key={`type_${index}`}>{item}</span>
+                    <>
+                        <span key={`type_${index}`} className="mx-1">{item}</span>
+                        {
+                            index < pokemonTypes.length - 1? <span>-</span> : null
+                        }
+                    </>
                 ))}
             </div>
         </div>
